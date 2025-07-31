706caf2b221d7f4480d58d0cf6fcfe6398acf273
Paul J. Davis <paul.davis@tiledb.com>
2025-07-31 17:09:06 +0000

    Add more regression tests

--- examples/check-regressions.rs
+++ examples/check-regressions.rs
@@ -16,6 +16,23 @@ fn test_2() -> Result<()> {
     run_test(vec![Command::Truncate(BoundedUsize::new(1))])
 }
 
+fn test_3() -> Result<()> {
+    run_test(vec![
+        Command::Truncate(BoundedUsize::new(2)),
+        Command::PRead(BoundedUsize::new(1), BoundedUsize::new(1))
+    ])
+}
+
+fn test_4() -> Result<()> {
+    run_test(vec![
+        Command::Truncate(BoundedUsize::new(2532034)),
+        Command::Write(BoundedUsize::new(2419266)),
+        Command::Truncate(BoundedUsize::new(662889)),
+        Command::Reopen,
+        Command::PRead(BoundedUsize::new(796278), BoundedUsize::new(1411041)),
+    ])
+}
+
 fn run_test(commands: Vec<Command>) -> Result<()> {
     let args = std::env::args().collect::<Vec<_>>();
     assert_eq!(args.len(), 4);
@@ -39,8 +56,10 @@ fn main() -> Result<()> {
     // unlikely to reach that exactly.
     MAX_FILE_SIZE.get_or_init(|| (size * 1024 * 1024) / 2);
 
-    //test_1()?;
+    test_1()?;
     test_2()?;
+    test_3()?;
+    test_4()?;
 
     eprintln!("\nSuccess!");
     Ok(())
